62955297d8bf8c5a6f2f3f2f60137043fa1f8a77
kadamsharayu9387-ai <kadamsharayu9387@gmail.com>
2026-02-02 21:16:47 +0400

    Create StringPalindrome.java

---
+++ Java/StringPalindrome.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class StringPalindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter string: ");
+        String s = sc.nextLine();
+
+        String rev = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            rev = rev + s.charAt(i);
+        }
+
+        if (s.equals(rev))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
