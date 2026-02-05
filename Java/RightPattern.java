3cf304360f3f809fd1060cbdcf325d68985327fa
kadamsharayu9387-ai <kadamsharayu9387@gmail.com>
2026-02-05 20:03:30 +0400

    Update RightPattern.java

--- Java/RightPattern.java
+++ Java/RightPattern.java
@@ -8,11 +8,11 @@ public class RightPattern {
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            // print spaces
+            
             for (int s = 1; s <= n - i; s++) {
                 System.out.print(" ");
             }
-            // print stars
+            
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
