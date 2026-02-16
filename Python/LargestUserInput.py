167b1cd5a57f5f4fee7902827c633aee043a123e
kadamsharayu9387-ai <kadamsharayu9387@gmail.com>
2026-02-16 21:32:01 +0400

    Create LargestUserInput.py

---
+++ Python/LargestUserInput.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print("First number is largest")
+elif b > a:
+    print("Second number is largest")
+else:
+    print("Both numbers are equal")
