fe47688355e38eea889c6113cfcc1c830bf06d10
kadamsharayu9387-ai <kadamsharayu9387@gmail.com>
2026-04-02 23:22:41 +0530

    Create SecondLargest.java

---
+++ Java/SecondLargest.java
@@ -0,0 +1,22 @@
+public class SecondLargest {
+    public static int findSecondLargest(int[] arr) {
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > first) {
+                second = first;
+                first = num;
+            } else if (num > second && num != first) {
+                second = num;
+            }
+        }
+
+        return second;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 4, 45, 99};
+        System.out.println(findSecondLargest(arr)); // 45
+    }
+}
